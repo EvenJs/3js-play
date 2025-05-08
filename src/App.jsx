a0ac619074b6d1ea8048cb368b88fbe3e10fc3bd
Johnson Wang <yuhang0229@gmail.com>
2025-05-08 15:00:48 +1000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Canvas } from "@react-three/fiber";
+
+const App = () => {
+  return (
+    <Canvas
+      style={{
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      123
+    </Canvas>
+  );
+};
+
+export default App;
