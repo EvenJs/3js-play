c618f4cfe9e2e899bd5647e7c2909b8a0d54ec43
Johnson Wang <yuhang0229@gmail.com>
2025-05-08 16:58:56 +1000

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { Navbar } from "./sections/Navbar";
 
 const App = () => {
   return (
     <main className="max-w-7xl mx-auto">
+      <Navbar />
       <h1 className="text-2xl text-white underline">hello</h1>
     </main>
   );
